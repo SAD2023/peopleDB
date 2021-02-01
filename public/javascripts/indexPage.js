6220d0bf8464761ecfc5ad1af00e7e88da180097
Sadman Chowdhury <64035273+SAD2023@users.noreply.github.com>
2021-01-31 21:31:52 -0500

    added comments to index page

--- public/javascripts/indexPage.js
+++ public/javascripts/indexPage.js
@@ -1,7 +1,9 @@
+// goes to the form page when first button is clicked
 document.getElementById("first-button").onclick = function () {
   window.location.pathname = "/form"
 }
 
+// goes to the people page when second button is clicked
 document.getElementById("second-button").onclick = function () {
   window.location.pathname = "/people"
 }
